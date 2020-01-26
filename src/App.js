711b97d1fd81eee217a8b080cc9a8179ac4c6223
Diego Plancarte <dplancarte7@gmail.com>
2020-01-25 23:21:50 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Compontent {
+  constructor(props){
+    super(props)
+    this.state = {
+        income: {
+          monthly: 0,
+          other: 0,
+        },
+        house_exps: {
+          mortgage: 0,
+          rent: 0,
+          hm_ins: 0,
+          utilities: 0,
+          phone: 0,
+          internet: 0,
+          hm_other: 0,
+        },
+        other_exps: {
+          food: 0,
+          clothes: 0,
+          entertainment: 0,
+          other: 0,
+        },
+        transp_exps: {
+          car: 0,
+          car_ins: 0,
+          gas: 0,
+          pub_tran: 0,
+          car_other: 0,
+        },
+        savings: {
+          emerg: 0,
+          invest: 0,
+          retire: 0,
+          sav_other: 0,
+        }
+    }
+  }
+  render(){
+    return (
+      <div>
+      </div>
+    )
+  }
 }
 
 export default App;
